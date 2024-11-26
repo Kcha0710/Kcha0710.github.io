585bd7571abc77fd7515d782996be121c656a0d2
ALONSO MORILLO <am7003456@gmail.com>
2024-11-26 16:58:47 -0500

    Paina Personal

---
+++ validacion.js
@@ -0,0 +1,14 @@
+function validacion(){
+const form = document.getElementById("nombre");
+        form.addEventListener('Enviar',function(event){
+            const nya = document.getElementById('nya').value;
+            const email = document.getElementById('email').value;
+            const numero = document.getElementById('numero').value;
+
+            if (nya=='' || email=='' || numero=='') {
+                alert('No puedes dejar campos vacios!');
+            }else{
+                alert('Formulario enviado correctamente!')
+            }
+        })
+    }
